b8201a46a1b112220da1cae08084ccfc536508f7
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-04-29 16:42:35 -0700

    add Engineer test suite

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,34 @@
-const { TestWatcher } = require('jest');
+const Test = require('jest');
 const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+  describe('Can generate an Engineer object', () => {
+    it('Can properly generate object', () => {
+      const Travis = new Engineer()
+      expect(typeof(Travis)).toBe("object")
+    })
+  })
+
+  describe('Can set constructor properties', () => {
+    it('Can set the GitHub', () => {
+      const gitname = 'ValiantThor92';
+      const newEngineer = new Engineer('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newEngineer.github).toBe(gitName);
+    })
+
+  })
+
+  describe('Can use methods', () => {
+    it('Can get gitHub property using getGitHub()', () => {
+      const name = 'ValiantThor92';
+      const newEngineer = new Engineer('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newEngineer.getGitHub()).toBe(gitName);
+    })
+
+    it('Returns Engineer when using getRole()', () => {
+      const role = 'Engineer'
+      const newEngineer = new Engineer('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newEngineer.getRole()).toBe(role)
+    })
+  })
+});
