64e7cf822a810201805627d07b1e7b0d2235b004
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-04-29 17:00:54 -0700

    create intern test suite

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,34 @@
-const { TestWatcher } = require('jest');
+const Test = require('jest');
 const Intern = require('../lib/Intern');
+
+describe('Intern', () => {
+  describe('Can generate an Intern object', () => {
+    it('Can properly generate object', () => {
+      const Travis = new Intern()
+      expect(typeof(Travis)).toBe("object")
+    })
+  })
+
+  describe('Can set constructor properties', () => {
+    it('Can set the school', () => {
+      const school = 'University of Oregon';
+      const newIntern = new Intern('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newIntern.school).toBe(school);
+    })
+
+  })
+
+  describe('Can use methods', () => {
+    it('Can get school property using getschool()', () => {
+      const school = 'University of Oregon';
+      const newIntern = new Intern('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newIntern.getSchool()).toBe(school);
+    })
+
+    it('Returns Intern when using getRole()', () => {
+      const role = 'Intern'
+      const newIntern = new Intern('Travis', 1, 'magnafunk72@gmail.com')
+      expect(newIntern.getRole()).toBe(role)
+    })
+  })
+});
